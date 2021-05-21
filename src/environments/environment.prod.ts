cab7430635ffa739c2b5c23fd8c064215d1b3577
SabbanData <sabban@datatrained>
2021-05-21 11:45:00 +0530

    for productons ready

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDtnHBaqrZ0Ifyu90h1OYHgSwgzl3D3kz8',
+    authDomain: 'livesession-6ed26.firebaseapp.com',
+    projectId: 'livesession-6ed26',
+    storageBucket: 'livesession-6ed26.appspot.com',
+    messagingSenderId: '414853175482',
+    appId: '1:414853175482:web:3efb3f6a1feb2d6f5e4a83',
+  },
+  ATTACHMENT_URL: 'https://files.datatrained.com/upload-files',
+  FILES_URL: 'https://files.datatrained.com',
+  time_api_url: 'http://worldclockapi.com/api/json/utc/now',
 };
