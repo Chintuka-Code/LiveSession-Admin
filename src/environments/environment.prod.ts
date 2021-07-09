5dcbe43de62a3d747f44135e2d26f6046fa4b83c
SabbanData <sabban@datatrained>
2021-07-09 17:54:11 +0530

    time

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   ATTACHMENT_URL: 'https://files.datatrained.com/upload-files',
   FILES_URL: 'https://files.datatrained.com',
-  time_api_url: 'http://worldclockapi.com/api/json/utc/now',
+  time_api_url: 'https://ops-api.datatrained.org/time',
   BASE_SERVER_URL: 'https://ops-api.datatrained.org',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,6 @@ export const environment = {
   production: false,
   ATTACHMENT_URL: 'https://files.datatrained.com/upload-files',
   FILES_URL: 'https://files.datatrained.com',
-  time_api_url: 'http://worldclockapi.com/api/json/utc/now',
+  time_api_url: 'https://ops-api.datatrained.org/time',
   BASE_SERVER_URL: 'http://localhost:4000',
 };
