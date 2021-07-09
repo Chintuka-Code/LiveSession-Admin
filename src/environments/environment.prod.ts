2d556d87c7be1bc64f96ca9f648fce8df2907afd
SabbanData <sabban@datatrained>
2021-07-09 16:15:23 +0530

    development

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   ATTACHMENT_URL: 'https://files.datatrained.com/upload-files',
   FILES_URL: 'https://files.datatrained.com',
   time_api_url: 'http://worldclockapi.com/api/json/utc/now',
-  BASE_SERVER_URL: 'http://localhost:4000',
+  BASE_SERVER_URL: 'https://ops-api.datatrained.org',
 };
