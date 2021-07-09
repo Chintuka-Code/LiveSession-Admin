9de1a2fd9243c420669941238bfe4d1b98780127
SabbanData <sabban@datatrained>
2021-07-09 15:28:49 +0530

    permission fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,5 +7,5 @@ export const environment = {
   ATTACHMENT_URL: 'https://files.datatrained.com/upload-files',
   FILES_URL: 'https://files.datatrained.com',
   time_api_url: 'http://worldclockapi.com/api/json/utc/now',
-  BASE_SERVER_URL: 'https://ops-api.datatrained.org/liveSessionBackend',
+  BASE_SERVER_URL: 'http://localhost:4000',
 };
