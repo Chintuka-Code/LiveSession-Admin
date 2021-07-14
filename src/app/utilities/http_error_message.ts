994a8cd6f801348720719b736ec57597a185717f
SabbanData <sabban@datatrained>
2021-07-14 09:09:53 +0530

    http session error handling

--- src/app/utilities/http_error_message.ts
+++ src/app/utilities/http_error_message.ts
@@ -2,6 +2,7 @@ export const ERROR_MESSAGE = (message, err) => {
   switch (message) {
     case 'jwt expired':
       localStorage.clear();
+      window.location.reload();
       return 'Your token has expired.Plese login';
 
     case `E11000 duplicate key error collection: liveSession.categories index: category_name_1 dup key: { category_name: "Academics Team" }`:
