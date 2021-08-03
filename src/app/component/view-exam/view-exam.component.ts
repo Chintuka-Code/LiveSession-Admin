fa3592ce29ea0473dbf8a27400c2f893bf24ac28
Anwarul Haque <anwarul@datatrained.com>
2021-08-03 18:27:33 +0530

    create and update exam

--- src/app/component/view-exam/view-exam.component.ts
+++ src/app/component/view-exam/view-exam.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
+import { ExamService } from '../../service/exam.service'
+import { ACTIVE_USER } from 'src/app/utilities/Decode_jwt';
+
 
 @Component({
   selector: 'app-view-exam',
@@ -7,9 +12,172 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewExamComponent implements OnInit {
   spinner: boolean = false;
-  constructor() { }
+  exam_list
+  user_profile: any;
+  menu_type: string;
+  items: any = [{ label: 'Actions', items: [] }];
+  question_bank_type_disabled: boolean = false;
+
+  constructor(
+    private router: Router,
+    private examService: ExamService,
+  ) { }
 
   ngOnInit(): void {
+    this.get_all_exam();
+    this.setMenu()
+  }
+
+
+  get_all_exam() {
+
+    this.examService.get_all_exam().subscribe(
+      (res: any) => {
+        console.log(res);
+        
+        this.exam_list = res.data;
+        console.log(this.exam_list);
+        this.spinner = false;
+      },
+      (error) => this.error_handler(error)
+    );
+  }
+
+  error_handler(error) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: error.errorMessage,
+    }).then(() => {
+      this.spinner = false;
+      this.router.navigate(['/main']);
+    });
+  }
+
+
+  exam_action(exam) {
+    // console.log(exam);
+    
+    switch (this.menu_type) {
+      case 'view':
+        this.view_exam_details();
+        break;
+      case 'edit':
+        this.examService.examDetails = exam;
+        this.examService.examDetails['selected_question'] = [];
+        let question_bank = exam.questions.map(data=>{
+          return data.question_bank_id;
+        })
+
+        let question = exam.questions.map(data=>{
+          return data.questions;
+        })
+
+        var merged = [].concat.apply([], question);        
+        
+        
+        this.examService.examDetails['selected_question']['question_bank'] = question_bank;
+        this.examService.examDetails['selected_question']['question'] = merged;
+        // this.examService.examDetails['exam_id'] = ;
+        let publish = this.examService.examDetails['publish'];
+        if(publish['start_date']){
+          publish['start_date'] = new Date(publish['start_date'])
+        }
+        if(publish['end_date']){
+          publish['end_date'] = new Date(publish['end_date'])
+        }
+        if(publish['start_time']){
+          publish['start_time'] = new Date(publish['start_time'])
+        }
+        if(publish['end_time']){
+          publish['end_time'] = new Date(publish['end_time'])
+        }
+        if(publish['slot_end_time']){
+          publish['slot_end_time'] = new Date(publish['slot_end_time'])
+        }
+        if(publish['slot_start_time']){
+          publish['slot_start_time'] = new Date(publish['slot_start_time'])
+        }
+        this.examService.examDetails['publish'] = publish;
+        console.log(this.examService.examDetails);
+        this.router.navigate(['/main/create-exam/form']);
+        break;
+      case 'disabled':
+        this.disabled_exam();
+        break;
+      case 'add-question':
+        // this.router.navigate(['/main/create-question'], {
+        //   queryParams: {
+        //     question_bank_id: question_bank._id,
+        //     question_bank_name: question_bank.question_bank_name,
+        //   },
+        // });
+        break;
+      default:
+        console.log('');
+    }
+
+    console.log('kllkj');
+    
+  }
+
+
+  setMenu() {
+    this.spinner = true;
+    this.user_profile = ACTIVE_USER();
+
+    if (!this.user_profile.permissions.includes('C00')) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Access Denied',
+      });
+      this.router.navigate(['/main']);
+      return;
+    }
+
+    // this.get_all_question_bank(this.question_bank_type_disabled);
+
+    this.items[0].items.push({
+      label: 'View',
+      icon: 'pi pi-eye',
+      command: () => {
+        this.menu_type = 'view';
+      },
+    });
+    this.items[0].items.push({
+      label: 'Disabled',
+      icon: 'pi pi-trash',
+      command: () => {
+        this.menu_type = 'disabled';
+      },
+    });
+
+    this.items[0].items.push({
+      label: 'Edit',
+      icon: 'pi pi-user-edit',
+      command: () => {
+        this.menu_type = 'edit';
+      },
+    });
+
+    this.items[0].items.push({
+      label: 'Add Question',
+      icon: 'pi pi-user-plus',
+      command: () => {
+        this.menu_type = 'add-question';
+      },
+    });
+  }
+
+
+  view_exam_details(){
+    console.log('lkjlkjl');
+    
+  }
+  disabled_exam(){
+    console.log('desable exam');
+    
   }
 
 }
