85a244b499815fd34a1959425f5440adee773733
SabbanData <sabban@datatrained.com>
2021-08-06 08:55:15 +0530

    formatting

--- src/app/component/exam-steps/exam-form/exam-form.component.ts
+++ src/app/component/exam-steps/exam-form/exam-form.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { ExamService } from '../../../service/exam.service'
+import { ExamService } from '../../../service/exam.service';
 import { Route } from '@angular/compiler/src/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-exam-form',
   templateUrl: './exam-form.component.html',
-  styleUrls: ['./exam-form.component.scss']
+  styleUrls: ['./exam-form.component.scss'],
 })
 export class ExamFormComponent implements OnInit {
-
   exam_form: any;
 
   submitted: boolean = false;
@@ -17,43 +16,27 @@ export class ExamFormComponent implements OnInit {
   is_timed: string[] = ['Yes', 'No'];
   is_timed_show = true;
 
-  constructor(
-    private examService: ExamService,
-    private router: Router
-
-  ) { }
+  constructor(private examService: ExamService, private router: Router) {}
 
   ngOnInit(): void {
     this.exam_form = this.examService.examDetails.exam_form;
     console.log(this.examService.examDetails);
-    
   }
 
-
-
-  timedChange(event){
-   
-      this.is_timed_show =!this.is_timed_show; 
-
-    
+  timedChange(event) {
+    this.is_timed_show = !this.is_timed_show;
   }
-
 
   nextPage() {
     this.examService.examDetails.exam_form = this.exam_form;
-    if (this.exam_form.exam_name && this.exam_form.is_timed ) {
-        
-        if(this.exam_form.is_timed == 'Yes' && !this.exam_form.exam_duration)
-          return;
-        else
-          this.router.navigate(['main/create-exam/instruction']);
-        
+    if (this.exam_form.exam_name && this.exam_form.is_timed) {
+      if (this.exam_form.is_timed == 'Yes' && !this.exam_form.exam_duration)
         return;
+      else this.router.navigate(['main/create-exam/instruction']);
+
+      return;
     }
-    
+
     this.submitted = true;
-}
-
-
-
+  }
 }
--- src/app/component/exam-steps/publish/publish.component.ts
+++ src/app/component/exam-steps/publish/publish.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ExamService } from '../../../service/exam.service'
+import { ExamService } from '../../../service/exam.service';
 import { Route } from '@angular/compiler/src/core';
 import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
@@ -7,25 +7,22 @@ import Swal from 'sweetalert2';
 @Component({
   selector: 'app-publish',
   templateUrl: './publish.component.html',
-  styleUrls: ['./publish.component.scss']
+  styleUrls: ['./publish.component.scss'],
 })
 export class PublishComponent implements OnInit {
-  batch_start_date
-  publish:{}
+  batch_start_date;
+  publish: {};
   spinner: boolean = false;
-  is_exam_id: boolean =false;
+  is_exam_id: boolean = false;
   sloat_end_time: Date;
   minDateValue: Date = new Date();
   addedTime: Date = new Date();
-  
-  constructor(
-    private examService: ExamService,
-    private router: Router
-  ) { }
+
+  constructor(private examService: ExamService, private router: Router) {}
 
   ngOnInit(): void {
     this.publish = this.examService.examDetails.publish;
-    if(this.examService.examDetails['_id']){
+    if (this.examService.examDetails['_id']) {
       this.is_exam_id = true;
     }
   }
@@ -35,40 +32,36 @@ export class PublishComponent implements OnInit {
   }
 
   examPublish() {
-
     console.log(this.examService.examDetails);
-   
-    
+
     const formData = Object.assign({}, this.examService.examDetails);
     delete formData.selected_question;
 
-    if(formData.access_setting['access_control'] == 'manual enter email'){
-    
-      delete formData.access_setting['menual_student']
+    if (formData.access_setting['access_control'] == 'manual enter email') {
+      delete formData.access_setting['menual_student'];
     }
 
     this.spinner = true;
-  console.log(formData);
-
-  if(this.is_exam_id){
-    delete formData['createdAt']
-    delete formData['updatedAt']
-    delete formData['__v']
     console.log(formData);
 
-    this.examService.update_exam(formData['_id'] , formData).subscribe(
-      (res: any) => {
+    if (this.is_exam_id) {
+      delete formData['createdAt'];
+      delete formData['updatedAt'];
+      delete formData['__v'];
+      console.log(formData);
+
+      this.examService.update_exam(formData['_id'], formData).subscribe(
+        (res: any) => {
           console.log(res);
-          
+
           Swal.fire({
             icon: 'success',
             title: 'Yeah...',
             text: 'Exam updated successfully',
-          }).then(() =>  this.router.navigate(['main/view-exam']));
+          }).then(() => this.router.navigate(['main/view-exam']));
           this.spinner = false;
 
           // this.spinner = false;
-        
         },
         (error) => {
           Swal.fire({
@@ -77,26 +70,22 @@ export class PublishComponent implements OnInit {
             text: error.errorMessage,
           }).then(() => {
             this.spinner = false;
-          
           });
         }
       );
-
-    
-  }else{
-    this.examService.create_exam(formData).subscribe(
-      (res: any) => {
+    } else {
+      this.examService.create_exam(formData).subscribe(
+        (res: any) => {
           console.log(res);
-          
+
           Swal.fire({
             icon: 'success',
             title: 'Yeah...',
             text: 'Exam created successfully',
-          }).then(() =>  this.router.navigate(['main/view-exam']));
+          }).then(() => this.router.navigate(['main/view-exam']));
           this.spinner = false;
 
           // this.spinner = false;
-        
         },
         (error) => {
           Swal.fire({
@@ -105,27 +94,21 @@ export class PublishComponent implements OnInit {
             text: error.errorMessage,
           }).then(() => {
             this.spinner = false;
-          
           });
         }
       );
     }
   }
 
-  onStartTimeSelect(value:Date){
-
-    if(this.examService.examDetails.exam_form['exam_duration']){
+  onStartTimeSelect(value: Date) {
+    if (this.examService.examDetails.exam_form['exam_duration']) {
       let duration = this.examService.examDetails.exam_form['exam_duration'];
-      this.addedTime = this.addMinutes(value, duration); 
+      this.addedTime = this.addMinutes(value, duration);
       this.publish['slot_end_time'] = this.addedTime;
-     
     }
-    
   }
-
 
   addMinutes(date, minutes) {
-    return new Date(date.getTime() + minutes*60000);
+    return new Date(date.getTime() + minutes * 60000);
   }
-
 }
