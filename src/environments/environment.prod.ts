07e064e5c94aa78427aa10282ab20aa5fccdd3e2
Anwarul Haque <anwarul@datatrained.com>
2021-08-15 12:14:05 +0530

    student bas url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,8 +3,8 @@ export const environment = {
   ATTACHMENT_URL: 'https://files.datatrained.com/upload-files',
   FILES_URL: 'https://files.datatrained.com',
   time_api_url: 'https://ops-api.datatrained.org/time',
-  BASE_SERVER_URL: 'https://ops-api.datatrained.org',
+  // BASE_SERVER_URL: 'https://ops-api.datatrained.org',
   // BASE_SERVER_URL: 'http://3.21.129.241',
-  // BASE_SERVER_URL: 'https://test.support.datatrained.org',
-  STUDENT_BASE_SERVER_URL: 'http://localhost:4200',
+  BASE_SERVER_URL: 'https://test.support.datatrained.org',
+  STUDENT_BASE_SERVER_URL: 'https://support.datatrained.org/dev',
 };
