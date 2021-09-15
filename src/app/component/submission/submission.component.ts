da8d4b5994b784d5b00aee5517854e3e3b9521a3
Anwarul Haque <anwarul@datatrained.com>
2021-09-15 10:47:52 +0530

    exam submission

--- src/app/component/submission/submission.component.ts
+++ src/app/component/submission/submission.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import Swal from 'sweetalert2';
 import { Router, ActivatedRoute } from '@angular/router';
 import { ExamService } from '../../service/exam.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-submission',
@@ -13,16 +14,19 @@ export class SubmissionComponent implements OnInit {
   spinner: boolean = false;
   exam_submission_list:any[] = [];
   exam_id:string = '';
+  answerUrl:string = '';
   constructor(private router: Router, private examService: ExamService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.exam_id = this.route.snapshot.paramMap.get('exam_id');
     this.getAllSubmission(this.exam_id);
+   this.answerUrl = `${environment.STUDENT_BASE_SERVER_URL}/exam/${this.exam_id}/show-answer/`
   }
 
 
   getAllSubmission(exam_id){
 
+    this.spinner = true;
     this.examService.get_all_submission(exam_id).subscribe(
       (res: any) => {
         console.log(res);
@@ -39,6 +43,12 @@ export class SubmissionComponent implements OnInit {
         });
       }
     );
+  }
+
+
+  viewAnswer(email){
+    
+    return `${environment.STUDENT_BASE_SERVER_URL}/exam/${this.exam_id}/show-answer/${email}`;
   }
 
 }
